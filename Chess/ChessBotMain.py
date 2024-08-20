6dda942012f4d2b09a52c68edc3cb6afab3969c6
huaden <161155310+huaden@users.noreply.github.com>
2024-08-20 13:12:58 -0400

    recent changes

--- Chess/ChessBotMain.py
+++ Chess/ChessBotMain.py
@@ -7,24 +7,25 @@ PIECE_VALUES = [0, 100, 350, 300, 500, 900, 0] #indexes 1-5: Pawn, Knight, Bisho
 
 
 def bestMove():
-    bestScore = -100000
+    bestScore = -100000#sets the best score to a small number so any score will be better
     bestMove = None
-    for move in board.legal_moves:
+    for move in board.legal_moves:#iterates through all possible moves
         board.push(move)
         score = 1
         score += miniMax(0, False, -100000, 100000)
-        board.pop()
+        board.pop()#sees what happens if the move is chosen then undos the move
         if(score >= bestScore):
             bestScore = score
             bestMove = move
-    if bestMove:
+
+    if bestMove:#chooses the best move
         board.push(bestMove)
     else:
         print("no move found")
 
 def scoreBoard():
     scores = 0
-    for square in chess.SQUARES:
+    for square in chess.SQUARES:#goes through all square of the board and adds the pieces left values
         piece = board.piece_at(square)
         if(piece):
             if (piece.color):
@@ -36,30 +37,31 @@ def scoreBoard():
 
 
 def miniMax(depth, isMaxing, alpha, beta):
+
     if board.is_game_over():
-        if(board.is_checkmate):
+        if(board.is_checkmate):#returns max points if you win or least points if you lose
             if(isMaxing):
                 return 100000000
             else:
                 return -100000000
         return 0
-    if depth == 4:
+    if depth == 4:#checks 4 moves ahead
         return scoreBoard()
 
-    if isMaxing:
+    if isMaxing:#is the bots maximize the score
         bestScore = -1000000
-        for move in board.legal_moves:
+        for move in board.legal_moves:#checks all legal moves available for the bot
             board.push(move)
             score = miniMax(depth+1, False, alpha, beta)
             board.pop()
 
-            alpha = max(alpha, beta)
-            bestScore = max(score, bestScore)
-            if beta <= alpha:
+            alpha = max(alpha, beta)#sets the alpha value so that if a worse move appears it will skip it
+            bestScore = max(score, bestScore)#chooses the best score
+            if beta <= alpha:#if the current move path isn't as good as the best one then skip it
                 break
 
         return bestScore
-    else:
+    else:#not the bots turn so they want to minimize the loses
         bestScore = 10000000
         for move in board.legal_moves:
             board.push(move)
@@ -68,14 +70,14 @@ def miniMax(depth, isMaxing, alpha, beta):
 
 
             bestScore = min(score, bestScore)
-            beta = min(bestScore, beta)
+            beta = min(bestScore, beta)#sets the current max score for this path
             if beta <= alpha:
                 break
         return bestScore
 
 
 
-while(board.is_game_over() is False):
+while(board.is_game_over() is False):#temp code to test
     print(board)
     print("\n\n\n")
     if(board.turn):
